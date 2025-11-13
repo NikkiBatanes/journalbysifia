7f6c558c294c5d79b5e965cf829d3310a83f85a3
Cascade Assistant <dev@local>
2025-11-13 09:51:09 +0800

    Match document view horizontal padding to stack view (16px)

--- src/screens/PlaybookDetailScreenNew.tsx
+++ src/screens/PlaybookDetailScreenNew.tsx
@@ -1985,7 +1985,7 @@ const createStyles = (theme: any) => StyleSheet.create<PlaybookDetailStyles>({
   docContentContainer: {
     paddingTop: 0,
     paddingBottom: 32,
-    paddingHorizontal: 32,
+    paddingHorizontal: 16,
     alignItems: 'center',
   },
   docContentContainerInner: {
