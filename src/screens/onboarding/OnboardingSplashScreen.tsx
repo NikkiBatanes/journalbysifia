8c81a45343dba1c54e8639e5b624eb33676b76e3
Cascade Assistant <dev@local>
2025-11-29 05:27:43 +0800

    Revert to correct transparent splash icon
    - Use siFiaTransparent.png as originally intended
    - React Native will automatically load @2x and @3x variants
    - These are the correct transparent files for the splash screen

--- src/screens/onboarding/OnboardingSplashScreen.tsx
+++ src/screens/onboarding/OnboardingSplashScreen.tsx
@@ -564,7 +564,7 @@ const OnboardingSplashScreen: React.FC<OnboardingSplashScreenProps> = ({ onCompl
       {/* Logo Section */}
       <View style={styles.logoSection}>
         <Image
-          source={require('../../../assets/icons/siFiaHeartWhite.png')}
+          source={require('../../../assets/icons/siFiaTransparent.png')}
           style={[styles.logoImage, { width: logoSize, height: logoSize }]}
           resizeMode="contain"
         />
