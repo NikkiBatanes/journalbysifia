25a28969a4cacf7b480caae524aff611473ef9ef
Cascade Assistant <dev@local>
2025-12-07 15:22:05 +0800

    Update logo to siFiav2Transparent in GeneratingPlaybookScreen

--- src/screens/GeneratingPlaybookScreen.tsx
+++ src/screens/GeneratingPlaybookScreen.tsx
@@ -352,7 +352,7 @@ const GeneratingPlaybookScreen: React.FC<Props> = ({ route, navigation }) => {
           <View style={styles.centerBlock}>
             <View style={styles.logoContainer}>
               <Image
-                source={require('../../assets/icons/siFiaAppIcon.png')}
+                source={require('../../assets/icons/siFiav2Transparent.png')}
                 style={styles.logo}
                 resizeMode="contain"
               />
