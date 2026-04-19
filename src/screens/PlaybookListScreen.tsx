2cb370ed237aeef5f9e85f223494044d1bc5062f
Cascade Assistant <dev@local>
2026-04-19 21:34:26 +0800

    Add gap below search bar by increasing marginBottom to 12

--- src/screens/PlaybookListScreen.tsx
+++ src/screens/PlaybookListScreen.tsx
@@ -2164,7 +2164,7 @@ const createStyles = (_theme: any) => StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 0,
     marginTop: 6,
-    marginBottom: 2,
+    marginBottom: 12,
     height: 42,
   },
   searchInputWrapper: {
