0c0b42c281de2129c4b9733d92155035e2a42a50
Cascade Assistant <dev@local>
2026-04-20 01:22:46 +0800

    Set modal paddingTop to 110

--- src/screens/PlaybookListScreen.tsx
+++ src/screens/PlaybookListScreen.tsx
@@ -2920,7 +2920,7 @@ const createStyles = (_theme: any) => StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.2)',
     justifyContent: 'flex-start',
-    paddingTop: 95,
+    paddingTop: 110,
     alignItems: 'flex-end',
     paddingRight: 16,
   },
