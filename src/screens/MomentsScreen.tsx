5648bd7971369a37ec70c950c97886917bb39d93
Cascade Assistant <dev@local>
2026-04-30 19:16:03 +0800

    Change Moments screen status bar to dark-content

--- src/screens/MomentsScreen.tsx
+++ src/screens/MomentsScreen.tsx
@@ -139,7 +139,7 @@ export const MomentsScreen: React.FC = () => {
 
   return (
     <SafeAreaView style={styles.container} edges={['left', 'right']}>
-      <StatusBar barStyle="light-content" backgroundColor={Colors.anchorBlue} />
+      <StatusBar barStyle="dark-content" backgroundColor={Colors.anchorBlue} />
 
       <View style={[styles.headerBar, { backgroundColor: Colors.anchorBlue }]}>
         <View style={styles.pageInner}>
