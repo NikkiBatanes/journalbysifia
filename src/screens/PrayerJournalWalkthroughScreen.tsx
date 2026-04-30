2fa4c29b213eae9d608254b3bf6d98c158a89751
Cascade Assistant <dev@local>
2026-04-30 18:16:11 +0800

    Add fontFamily to completionInput to use theme font
    - Add fontFamily: Fonts.regular to completionInput style
    - Ensures opening and closing input fields use theme font
    - TextInput now respects theme font system

--- src/screens/PrayerJournalWalkthroughScreen.tsx
+++ src/screens/PrayerJournalWalkthroughScreen.tsx
@@ -1976,6 +1976,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 12,
     marginTop: 8,
+    fontFamily: Fonts.regular,
   },
   completionSectionSmall: {
     marginBottom: 0,
